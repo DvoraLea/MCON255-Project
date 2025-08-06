2ea1a4e3e2a82b3cc966cd7509f2f982b2e694bf
DvoraLeah <dshprint@student.touro.edu>
2025-08-06 14:06:42 -0400

    ic luncher icon, add task dialog, java logic

--- Planner/app/src/main/java/com/example/planner/activities/MainActivity.java
+++ Planner/app/src/main/java/com/example/planner/activities/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.planner;
+package com.example.planner.activities;
 
 import android.os.Bundle;
 
+import com.example.planner.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
